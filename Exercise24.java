1afb163dedbca5e975aa2f28288c27b098bca867
ryaber-ux <ryaan.berkonti58@student.qcc.cuny.edu>
2025-03-13 10:42:34 -0400

    Update and rename Ex15_fuctions.java to Exercise24.java

---
+++ Exercise24.java
@@ -0,0 +1,18 @@
+public class Exercise24 {
+  public static int minArray(int [] a){
+    int minimum = a[0];
+    for(int i=0; =i<a.length;i++){
+      if(int i=0; i<a.length;i++){
+        if(minimum > a[a])
+          minimum = a[i];
+      }
+      return minimum;
+    }
+  }
+  public static void main(String[] args){
+    int [] a = {3,4,5,2,3}
+    System.out.println(minArray(a));
+    int [] b = {-1,3,4-10,5,2,20,3};
+    System.out.println(minArray(b));
+  }
+}
