c7fdbb87872601a2152c2a66b7a07353c4e17aa4
ryaber-ux <ryaan.berkonti58@student.qcc.cuny.edu>
2025-05-12 21:51:09 -0400

    Create Shape.java
    name: A protected string variable to store the name of the shape.
    Constructor: Takes name as an argument and assigns it to the name variable.
    Abstract methods:
    area(): return the area of the shape.
    perimeter(): return the perimeter.
    display(): print out shape details.

---
+++ Shape.java
@@ -0,0 +1,11 @@
+public abstract class Shape {
+    protected String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+    public abstract void display();
+}
